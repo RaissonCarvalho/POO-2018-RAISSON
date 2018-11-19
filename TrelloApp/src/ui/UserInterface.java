5f9fe19e10ccb249fc02e2e91e0d4de22eeb698b
RaissonCarvalho <raissoncarvalho94@gmail.com>
2018-11-19 14:37:52 -0300

    Modelando Interface com Usuario

--- TrelloApp/src/ui/UserInterface.java
+++ TrelloApp/src/ui/UserInterface.java
@@ -1,7 +1,26 @@
 package ui;
 
+import javax.swing.*;
+
 public class UserInterface {
 
-    
+    public static int readMenuOption(){
+        String menu = "--------  TRELLO  --------";
+        menu += "\n\n1 - Criar novo Quadro";
+
+        String strOpcao = JOptionPane.showInputDialog(null,menu);
+
+        return Integer.parseInt(strOpcao);
+    }
+
+    public static void showMsg(String message){
+
+        JOptionPane.showMessageDialog(null, message);
+
+    }
+
+    public static String requestTitle(){
+        return JOptionPane.showInputDialog(null, "Digite o título");
+    }
 
 }
