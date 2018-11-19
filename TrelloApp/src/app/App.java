03dbd833945a2cb78eaacd6948fbefad5c843d9f
RaissonCarvalho <raissoncarvalho94@gmail.com>
2018-11-19 14:50:08 -0300

    Criando quadros

--- TrelloApp/src/app/App.java
+++ TrelloApp/src/app/App.java
@@ -2,10 +2,52 @@ package app;
 
 import model.Board;
 import model.List;
+import ui.UserInterface;
+
+import java.util.ArrayList;
 
 public class App {
 
     public static void main(String[] args) {
+
+        ArrayList<Board> boards = new ArrayList<>();
+
+        final int CREAT_BOARD =1;
+        final int SAIR = 0;
+
+        while (true){
+
+            int opcao = UserInterface.readMenuOption();
+
+            switch (opcao){
+
+                case CREAT_BOARD:{
+
+                    try {
+
+                        String title = UserInterface.requestTitle();
+
+                        Board board = new Board(title, "private");
+                        boards.add(board);
+
+                    }catch (NullPointerException e){
+
+                        UserInterface.showMsg("Erro" + e.getMessage());
+
+                    }
+                    break;
+                }
+
+                case SAIR:{
+                    return;
+                }
+
+                default: {
+                    UserInterface.showMsg("Digite um opção válida");
+                }
+            }
+
+        }
 
     }
 }
