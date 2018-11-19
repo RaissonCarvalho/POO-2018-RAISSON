abf2ee52a19a4eb9096209c6973e6133e05eeb1e
RaissonCarvalho <raissoncarvalho94@gmail.com>
2018-11-19 15:39:40 -0300

    Criando metodo addBoard

--- TrelloApp/src/service/Service.java
+++ TrelloApp/src/service/Service.java
@@ -1,4 +1,32 @@
 package service;
 
+import model.Board;
+
+import java.util.ArrayList;
+
 public class Service {
+
+    ArrayList<Board> boards = new ArrayList<>();
+
+    public ArrayList<Board> getBoards() {
+        return boards;
+    }
+
+    public void createBoard(String title, String tipo){
+
+        Board board = new Board(title, tipo);
+        boards.add(board);
+
+    }
+
+    public int searchIndexBoard(){
+
+        for (int i = 0; i < boards.size(); i++){
+
+            
+
+        }
+
+    }
+
 }
