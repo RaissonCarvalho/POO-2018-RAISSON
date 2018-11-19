cbf1f5e89c54c9518e4340772d79ddf8b38ad5bf
RaissonCarvalho <raissoncarvalho94@gmail.com>
2018-11-19 16:26:25 -0300

    Implementando´método searchBoardByName

--- TrelloApp/src/service/Service.java
+++ TrelloApp/src/service/Service.java
@@ -14,19 +14,45 @@ public class Service {
 
     public void createBoard(String title, String tipo){
 
+
         Board board = new Board(title, tipo);
         boards.add(board);
 
     }
 
-    public int searchIndexBoard(){
+    public int searchIndexBoard(String title){
+
+        int index = 0;
 
         for (int i = 0; i < boards.size(); i++){
 
-            
+            if (boards.get(i).getTitulo().equals(title)){
+
+                index = i;
+                break;
+
+            }
+
+        }
+        return index;
+    }
+
+    public Board searchByTitle(String title){
+
+        Board board = null;
+
+        for (int i = 0; i < boards.size(); i++){
+
+            if (boards.get(i).getTitulo().equals(title)){
+
+                board = boards.get(i);
+                break;
+
+            }
 
         }
 
+        return board;
     }
 
 }
