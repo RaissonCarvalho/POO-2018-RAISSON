e21be061c9996b6bc05ff402d785953ffbe52a21
RaissonCarvalho <raissoncarvalho94@gmail.com>
2018-11-20 13:53:32 -0300

    Criando Classe Log

---
+++ TrelloApp/src/model/Log.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Log {
+
+    private String descricao;
+
+    public Log(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
